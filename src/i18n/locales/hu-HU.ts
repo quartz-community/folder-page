3e8972bcb3a3766233c0f2e3daba7294eb8b79b2
saberzero1 <github@emilebangma.com>
2026-04-04 02:11:04 +0200

    i18n: port locale translations from Quartz v4

--- src/i18n/locales/hu-HU.ts
+++ src/i18n/locales/hu-HU.ts
@@ -2,8 +2,7 @@ export default {
   pages: {
     folderContent: {
       folder: "Mappa",
-      itemsUnderFolder: ({ count }: { count: number }) =>
-        `Ebben a mappában ${count} elem található.`,
+      itemsUnderFolder: ({ count }: { count: number }) => `Ebben a mappában ${count} elem található.`,
     },
   },
   components: {},
